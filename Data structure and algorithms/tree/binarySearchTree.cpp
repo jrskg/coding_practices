d5ae0fd9f4d4472df7d555567ab93ed43ba332e2
suraj946 <sgsuraj150@gmail.com>
2024-02-06 14:01:47 +0545

    solved more BST questions

--- Data structure and algorithms/tree/binarySearchTree.cpp
+++ Data structure and algorithms/tree/binarySearchTree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<queue>
+#include<vector>
 using namespace std;
 
 class Node{
@@ -158,6 +159,35 @@ pair<int, int>inorderPredSucc(Node* root, int key, int p, int s){
     }
 }
 
+pair<int, int>inorderPredSuccIterative(Node* root, int key){
+    int pred = -1;
+    int succ = -1;
+
+    Node* temp = root;
+    while(temp->data != key){
+        if(temp->data < key){
+            pred = temp->data;
+            temp = temp->right;
+        }else{
+            succ = temp->data;
+            temp = temp->left;
+        }
+    }
+
+    Node* leftTree = temp->left;
+    while(leftTree != NULL){
+        pred = leftTree->data;
+        leftTree = leftTree->right;
+    }
+
+    Node* rightTree = temp->right;
+    while(rightTree != NULL){
+        succ = rightTree->data;
+        rightTree = rightTree->left;
+    }
+    return {pred, succ};
+}
+
 Node* deleteFromBST(Node* root, int key){
     if(root == NULL){
         return root;
@@ -196,6 +226,118 @@ Node* deleteFromBST(Node* root, int key){
     }
 }
 
+int kThSmallest(Node* root, int k, int &i){
+    if(root == NULL){
+        return -1;
+    }
+
+    int leftAns = kThSmallest(root->left, k, i);
+    if(leftAns != -1){
+        return leftAns;
+    }
+    i = i+1;
+    if(i == k){
+        return root->data;
+    }
+
+    return kThSmallest(root->right, k, i);
+}
+
+void getInorderData(Node* root, vector<int> &v){
+    if(root == NULL){
+        return;
+    }
+    getInorderData(root->left, v);
+    v.push_back(root->data);
+    getInorderData(root->right, v);
+}
+bool twoSumInBST(Node* root, int target) {
+	vector<int>inorderVal;
+    getInorderData(root, inorderVal);
+
+    int i = 0, j = inorderVal.size()-1;
+    int sum;
+
+    while(i < j){
+        sum = inorderVal[i] + inorderVal[j];
+        if(sum == target){
+            return true;
+        }
+        else if(sum < target){
+            i++;
+        }
+        else{
+            j--;
+        }
+    }
+    return false;
+}
+
+void getInorderRoot(Node* root, vector<Node*> &v){
+    if(root == NULL){
+        return;
+    }
+    getInorderRoot(root->left, v);
+    v.push_back(root);
+    getInorderRoot(root->right, v);
+}
+
+Node* flatten(Node* root)
+{
+    vector<Node*>inorder;
+    getInorderRoot(root, inorder);
+    int i;
+    for(i = 0; i < inorder.size()-1; i++){
+        inorder[i]->left = NULL;
+        inorder[i]->right = inorder[i+1];
+    }
+    inorder[i]->left = NULL;
+    inorder[i]->right = NULL;
+
+    return inorder[0];
+}
+
+Node* buildBalanced(vector<int>v, int s, int e){
+    if(s>e){
+        return NULL;
+    }
+    int mid = (s+e)/2;
+    Node* root = new Node(v[mid]);
+    root->left = buildBalanced(v, s, mid-1);
+    root->right = buildBalanced(v, mid+1, e);
+
+    return root;
+}
+Node* convertIntoBalanced(Node* root){
+    vector<int> inorder;
+    getInorderData(root, inorder);
+
+    return buildBalanced(inorder, 0, inorder.size()-1);
+}
+
+Node* buildFormPreorder(vector<int> &preorder, int mini, int maxi, int &i){
+    if(i >= preorder.size()){
+        return NULL;
+    }
+    if(preorder[i] < mini || preorder[i] > maxi){
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[i++]);
+    root->left = buildFormPreorder(preorder, mini, root->data, i);
+    root->right = buildFormPreorder(preorder, root->data, maxi, i);
+    return root;
+}
+
+Node* preorderToBST(vector<int> &preorder){
+    int mini = INT16_MIN;
+    int maxi = INT16_MAX;
+    int i = 0;
+
+    return buildFormPreorder(preorder, mini, maxi, i);
+}
+
+
 int main() {
     // cout<<"Okay"<<endl;
 
@@ -212,18 +354,25 @@ int main() {
     // cout<<minVal(root)->data<<endl;
     // cout<<maxVal(root)->data<<endl;
 
-    // int n;
-    // cout<<"Enter n : ";
-    // cin>>n;
-    // pair<int, int> predSucc = inorderPredSucc(root, n, -1, -1);
-    // cout<<"Predessor : "<<predSucc.first<<endl;
-    // cout<<"Successor : "<<predSucc.second<<endl;
-
     int n;
     cout<<"Enter n : ";
     cin>>n;
-    root = deleteFromBST(root, n);
-    levelOrderTraversel(root);
+    // pair<int, int> predSucc = inorderPredSucc(root, n, -1, -1);
+    pair<int, int> predSucc = inorderPredSuccIterative(root, n);
+    cout<<"Predessor : "<<predSucc.first<<endl;
+    cout<<"Successor : "<<predSucc.second<<endl;
+
+    // int n;
+    // cout<<"Enter n : ";
+    // cin>>n;
+    // root = deleteFromBST(root, n);
+    // levelOrderTraversel(root);
+
+    // int key;
+    // cout<<"Enter key : ";
+    // cin>>key;
+    // int i = 0;
+    // cout<<endl<<key<<" th smallest element : "<<kThSmallest(root, key, i)<<endl;
 
     return 0;
 }
